617bcf3bf8272b884971a0b22c7493382da4bb54
abdikarim hussein <abdikarim.hussein557@gmail.com>
2022-10-24 21:05:54 +0100

    render

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 const CatModel = require('./cats');
 
-const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT, Node_ENV, DB_PASSWORD } = process.env;
+const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT, Node_ENV } = process.env;
 
 const setUpDatabase = () => {
         const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
         host: DB_HOST,
-        port:DB_PORT
+        port:DB_PORT,
         dialect: Node_ENV == "production" ? "postgres" : "mysql"
         });
         const Cat = CatModel(connection, Sequelize);
